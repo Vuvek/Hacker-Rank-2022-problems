249b14d83e1fc4c95e6902d689ab100ade8fe6e0
Vivek kumar <vivek.9718470@gmail.com>
2022-03-14 11:56:54 +0530

    third Commit

--- String Formating.py
+++ String Formating.py
@@ -1,20 +1,33 @@
 
-# def print_formatted(number):
-#     for i in range(1,number+1):
-#         d = str(i).rjust(len(bin(number)[2:]),' ')
-#         o = oct(i)[2:].rjust(len(bin(number)[2:]),' ')
-#         h = (hex(i)[2:].rjust(len(bin(number)[2:]),' ')).upper()
-#         b = bin(i)[2:].rjust(len(bin(number)[2:]),' ')
-#         print("{} {} {} {}".format(d,o,h,b))
+# By First Method
+'''
 
-# if __name__ == '__main__':
-#     n = int(input())
-#     print_formatted(n)
+def print_formatted(number):
+    for i in range(1,number+1):
+        d = str(i).rjust(len(bin(number)[2:]),' ')
+        o = oct(i)[2:].rjust(len(bin(number)[2:]),' ')
+        h = (hex(i)[2:].rjust(len(bin(number)[2:]),' ')).upper()
+        b = bin(i)[2:].rjust(len(bin(number)[2:]),' ')
+        print("{} {} {} {}".format(d,o,h,b))
 
-def main(n):
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
+
+'''
+
+
+
+
+
+
+def print_formatted(n):
     for i in range(1,n+1):
-        hexadecimal_result = format(i,"01X")
-        s = str(bin(n+1)[2:])
+        # hexadecimal_result = format(i,"01X")
+        s = str(bin(n)[2:])
         l=len(s)
-        print(f"{i:>{l}} {oct(i)[2:]:>{l}} {hexadecimal_result:>{l}} {bin(i)[2:]:>{l}} ")
-main(15)
+        print(f"{i:>{l}} {oct(i)[2:]:>{l}} {hex(i)[2:].upper():>{l}} {bin(i)[2:]:>{l}} ")
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
