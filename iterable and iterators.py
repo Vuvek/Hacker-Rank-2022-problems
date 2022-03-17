5174c79989121ebdcc76323bde51b0ae708352ee
Vivek kumar <vivek.9718470@gmail.com>
2022-03-17 20:25:54 +0530

    commited

--- iterable and iterators.py
+++ iterable and iterators.py
@@ -1,17 +1,18 @@
 
-from itertools import combinations
-from math import comb
 
-# n = int(input("Enter : "))
-# l = list(input().split())
-# k = int(input("Enter the index : "))
-k = ['a','a','c','d']
-combo = list(combinations(k,2))
+from itertools import combinations
+
+
+n = int(input("Enter : "))
+l = list(input().split())
+k = int(input("Enter the index : "))
+# k = ['a','a','c','d']
+combo = list(combinations(l,k))
 counter = 0
 for i in combo:
     if 'a' in i:
         counter += 1
-print(combo)
-print(counter)
+# print(combo)
+# print(counter)
 prob = counter/len(combo)
 print(prob)
